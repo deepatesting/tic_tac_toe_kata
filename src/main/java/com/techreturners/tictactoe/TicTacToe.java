d59100f3278895cc2f25d6e3c2f2229f26fc6c1c
deepatesting <kspdeepa82@gmail.com>
2022-04-08 19:52:41 +0100

    Added test case for player 2 to win.

--- src/main/java/com/techreturners/tictactoe/TicTacToe.java
+++ src/main/java/com/techreturners/tictactoe/TicTacToe.java
@@ -6,8 +6,10 @@ public class TicTacToe {
 
     //char[][] board = new char[3][3];
     int countplayerOne = 0;
+    int countplayerTwo = 0;
 
     public String play(int[][] board){
+
 
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++){
@@ -15,6 +17,11 @@ public class TicTacToe {
                     countplayerOne++;
                     if (countplayerOne==3)
                         return "Player 1 wins!";
+                }
+                else if(board[i][j] == 2){
+                    countplayerTwo++;
+                    if (countplayerTwo==3)
+                        return "Player 2 wins!";
                 }
 
             }
--- src/test/java/com/techreturners/tictactoe/TicTacToeTest.java
+++ src/test/java/com/techreturners/tictactoe/TicTacToeTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class TicTacToeTest {
 
     @Test
-    public void checkIfNineFieldsExist(){
+    public void checkIfPlayerOneWinsWhenFirstRowFull(){
         //Arrange
         TicTacToe ticTacToe = new TicTacToe();
 
@@ -18,5 +18,19 @@ public class TicTacToeTest {
 
         //Assert
         Assert.assertEquals("Player 1 wins!", response);
+    }
+    @Test
+    public void checkIfPlayerTwoWinsWhenSecondRowFull(){
+        //Arrange
+        TicTacToe ticTacToe = new TicTacToe();
+
+        int[][] board = {{1,2,0}, {1,2,0}, {0,2,1}};
+        String response = "";
+
+        //Act
+        response = ticTacToe.play(board);
+
+        //Assert
+        Assert.assertEquals("Player 2 wins!", response);
     }
 }
